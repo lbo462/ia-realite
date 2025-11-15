51a0e2214748d94b2213cfcf8b7e15deb80b274e
lbo462 <leobonnaire@icloud.com>
2025-11-15 22:05:34 +0100

    rolled back some changes to avoid later conflicts

--- src/ia_realite/room.py
+++ src/ia_realite/room.py
@@ -23,6 +23,7 @@ class Room:
     # dans Room
     def _build_room_system_prompt(self, subject: str) -> str:
         return f"ROOM RULES: /n In this room, there are entites with different personalities. They talk to each other and try to give different point of view concerning the same CONVERSATION SUBJECT of discussion. You are one of them. When you are told: It's your turn, you will receive the current state of the conversation, and you can continue talking, giving your own point of view. Keep your answers short (4 sentences MAX) and relevant to the current topic of discussion. /n CONVERSATION SUBJECT: {subject}."
+
     def add_entity(self, entity_name: str, entity_system_prompt: str):
         system_message = f"{self.room_system_prompt}\n YOU ARE {entity_name}: {entity_system_prompt}"
         self.entities.append(
